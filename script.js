d4da97202876a75d8ccf9f893e7b15e487374e9d
Jeofferson Dela Pena <jeoffersond@gmail.com>
2020-05-26 14:06:23 +0800

    shortened js

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 $(document).ready(function () {
 
-	$(document).click(function (event) {
-
-		var clickover = $(event.target);
-
-		var navbarCollapse = $(".navbar-collapse");
-		var isOpened = !navbarCollapse.hasClass("navbar-collapse in");
-
-		if (isOpened && clickover.parents("bg-dark")) {
-
-			navbarCollapse.collapse("hide");
-
-		}
-
-	});
+	$(document).click(() => $(".navbar-collapse").collapse("hide"));
 
 });
