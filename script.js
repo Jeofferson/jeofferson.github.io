0185d5d6a94d23576a184c89622bdeb61b58e3a9
Jeofferson Dela Pena <jeoffersond@gmail.com>
2020-05-30 10:43:44 +0800

    Enabled smoothscroll for safari

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
 	$(document).click(() => $(".navbar-collapse").collapse("hide"));
 
 
+	const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+
+		speed: 1000,
+		speedAsDuration: true
+
+	});
+
+
 });
