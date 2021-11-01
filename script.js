e222bf303a5b338ba1aab4aa0f4a9b28ac837c28
Jeofferson Dela Peña <jeoffersond@gmail.com>
2021-11-01 09:59:57 +0800

    Add multiple awards, events, etc

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 $(document).ready(function () {
+  AOS.init({
+    offset: 200,
+    duration: 1000,
+  });
 
+  const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true,
+  });
 
-	AOS.init({
-		offset: 200,
-		duration: 1000
-	});
-
-
-	const scroll = new SmoothScroll('.navbar a[href*="#"]', {
-		speed: 1000,
-		speedAsDuration: true
-	});
-
-
-	$(document).click(() => $(".navbar-collapse").collapse("hide"));
-
-
+  $(document).click(() => $('.navbar-collapse').collapse('hide'));
 });
